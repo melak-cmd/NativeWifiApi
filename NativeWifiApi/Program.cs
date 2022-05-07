7451b8145edd3911dadea4f46d8f9cc429822f5b
Amine <amine.kaouani@outlook.com>
2022-05-07 15:26:26 +0200

    Add project files.

---
+++ NativeWifiApi/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NativeWifiApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            platformInvoke.wifi wifiAPI = new platformInvoke.wifi();
+            wifiAPI.EnumerateNICs();
+            Console.ReadLine();
+        }
+    }
+
+}
+
